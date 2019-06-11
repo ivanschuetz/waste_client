b04498979e239771093bb8cf0be96a785b3b98b4
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-11 09:18:00 +0200

    Add a dummy leaflet map with a marker to homepage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 import SearchBox from "./SearchBox";
 import ItemSuggestions from "./ItemSuggestions";
 import ItemSearch from "./SearchResults";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from 'leaflet';
+
+const markerIcon = new L.Icon({
+    iconUrl: require('./marker.svg'),
+    iconRetinaUrl: require('./marker.svg'),
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(30, 30),
+    className: 'leaflet-div-icon'
+});
 
 const App = () => {
-    console.log('redering app');
-
     const [suggestions, setSuggestions] = useState([]);
     const [selectedSuggestion, setSelectedSuggestion] = useState(null);
 
@@ -20,15 +32,21 @@ const App = () => {
         setSelectedSuggestion(suggestion)
     };
 
-    const bar = selectedSuggestion && <ItemSearch item={selectedSuggestion} />
-    console.log('selectedSuggestion: ' + selectedSuggestion);
-    console.log('bar: ' + bar);
-
     return (
         <div className="App">
             <SearchBox onResults={handleSuggestions}/>
             <ItemSuggestions suggestions={suggestions} onClick={onSuggestionClick} />
             {selectedSuggestion && <ItemSearch suggestion={selectedSuggestion} />}
+            <Map center={[51.505, -0.09]} zoom={13} style={{height: 380}}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]} opacity={1} icon={markerIcon}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </Map>
         </div>);
 };
 
