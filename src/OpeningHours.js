0a7adbfc130e4c4fb5d4a0e7ab9185ebeba54f50
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-15 22:57:37 +0200

    Highlight opening hour for current time

--- src/OpeningHours.js
+++ src/OpeningHours.js
@@ -1,15 +1,32 @@
 import React from "react";
 import './App.css';
+import moment from "moment";
 
 const OpeningHoursTable = ({openingHoursList}) => <table className="opening-times-table">
     {openingHoursList.map((openingHours) => OpeningHoursRow(openingHours))}
 </table>;
 
 const OpeningHoursRow = (openingHours) =>
-    <tr>
+    <tr className={rowClassName(openingHours["start"], openingHours["end"])}>
         <td>{formatWeekdayStr(openingHours["weekday"])}</td>
         <td>{formatTime(openingHours["start"])} - {formatTime(openingHours["end"])}</td>
     </tr>;
+
+const nowIsBetween = (start, end) => {
+    const now = moment();
+    const timeFormat = 'hh:mm';
+    const startMoment = moment(start, timeFormat);
+    const endMoment = moment(end, timeFormat);
+    return now.isBetween(startMoment, endMoment);
+};
+
+const rowClassName = (start, end) => {
+    if (nowIsBetween(start, end))  {
+        return 'opening-hours-row-now'
+    } else {
+        return 'opening-hours-row-default'
+    }
+};
 
 const formatTime = (time) => `${time["hours"]}:${time["mins"]}`;
 
