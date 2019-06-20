284775e774dbac1fcd7f69381eb255400effac19
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-20 10:26:19 +0200

    Add item missing link

--- src/ItemSuggestions.js
+++ src/ItemSuggestions.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './App.css';
+import {useTranslation} from "react-i18next";
 
 const ItemSuggestions = ({suggestions, onClick}) => {
+    const { t } = useTranslation();
 
     const listItems = () => {
         return suggestions.map(suggestion =>
             <li key={suggestion.id} className="suggestion-item">
                 <div className="suggestion" onClick={() => onClick(suggestion)}>{suggestion.name}</div>
+            </li>
+        ).concat(
+            <li className="missingItemListItem">
+                <a className="missingItemLink" href="mailto:ivanschuetz@gmail.com&su=fooo" target="_blank">{t('suggestion_not_found')}</a>
             </li>
         );
     };
