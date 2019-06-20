b47c8b33238ec37682793165433aaac55d179561
Ivan Schuetz <ivanschuetz@gmail.com>
2019-06-20 22:58:59 +0200

    Add suggestions keyboard navigation

--- src/ItemSuggestions.js
+++ src/ItemSuggestions.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import {useTranslation} from "react-i18next";
 
 const ItemSuggestions = ({suggestions, onClick}) => {
     const { t } = useTranslation();
+    const [highlightedIndex, setHighlightedIndex] = useState(-1);
+
+    const keepInBounds = (newIndex) => Math.max(Math.min(newIndex, suggestions.length - 1), 0);
+
+    useEffect(() => {
+        const keyDownListener = ({key}) => {
+            if (key === "ArrowDown") {
+                setHighlightedIndex(keepInBounds(highlightedIndex + 1));
+            } else if (key === "ArrowUp") {
+                setHighlightedIndex(keepInBounds(highlightedIndex - 1));
+            } else if (key === "Enter") {
+                onClick(suggestions[highlightedIndex])
+            }
+        };
+
+        window.addEventListener('keydown', keyDownListener);
+        // Remove event listeners on cleanup
+        return () => {
+            window.removeEventListener('keydown', keyDownListener);
+        };
+    }, [highlightedIndex]); // Empty array ensures that effect is only run on mount and unmount
+
+    const suggestionClassName = (index) => {
+        if (index === highlightedIndex) {
+            return "suggestion-highlighted"
+        } else {
+            return "suggestion"
+        }
+    };
 
     const listItems = () => {
-        return suggestions.map(suggestion =>
+        return suggestions.map((suggestion, index) =>
             <li key={suggestion.id} className="suggestion-item">
-                <div className="suggestion" onClick={() => onClick(suggestion)}>{suggestion.name}</div>
+                <div className={suggestionClassName((index))} onClick={() => onClick(suggestion)}>{suggestion.name}</div>
             </li>
         ).concat(
             <li className="missingItemListItem" key='suggestion_not_found'>
