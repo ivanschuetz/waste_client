1d026dd9c1082d3a15a87f698bf7e9c4c0226ca2
khimsh <i.khimsh@gmail.com>
2019-06-23 15:42:35 +0400

    update: modal scrollable

--- src/Modal.js
+++ src/Modal.js
@@ -1,16 +1,20 @@
 import * as ReactDOM from "react-dom";
 import React from "react";
 
+
 const Modal = ({ title, children, onCloseClick }) => (
     ReactDOM.createPortal(
         <div className="modal">
             <div className="modal-content">
-            <div className="modal-topbar">
-                <h2 className="modal-topbar-title">{title}</h2>
-                <p className="modal-topbar-x" onClick={() => onCloseClick()}>X</p>
-            </div>
-                <div style={{ clear: "left" }} />
-                {children}
+                <div className="modal-topbar">
+                    <h2 className="modal-topbar-title">{title}</h2>
+                    <p className="modal-topbar-x" onClick={() => onCloseClick()}>X</p>
+                </div>
+                <div className="modal-bottombar">
+
+                    <div style={{ clear: "left" }} />
+                    {children}
+                </div>
             </div>
         </div>,
         document.getElementById('modal-root')
