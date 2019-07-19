d9cdf50196b16cc1808e1dd0c99df9128cf0f9c7
Ivan Schuetz <ivanschuetz@gmail.com>
2019-07-19 20:21:05 +0200

    Show only car route option, as default

--- src/RecipientsMap.js
+++ src/RecipientsMap.js
@@ -158,13 +158,17 @@ const RecipientsMap = ({ recipients }) => {
             {/*    {container["company"]}*/}
             {/*</a>*/}
             <div className="p-container-popup-routes" />
-            <span style={{ float: "left", marginRight: 5 }}>Route:</span>
-            {[
-                transportImg(require("./walk.svg"), "Walking", routeLink(myLoc, lat, lon, "walking")),
-                transportImg(require("./bike.svg"), "Bike", routeLink(myLoc, lat, lon, "bicycling")),
-                transportImg(require("./transit.svg"), "Transit", routeLink(myLoc, lat, lon, "transit")),
-                transportImg(require("./car.svg"), "Car", routeLink(myLoc, lat, lon, "driving")),
-            ]}
+            <a className="map-popup-route-link"
+               href={routeLink(myLoc, lat, lon, "driving")}
+               target="_blank"
+               rel="noopener noreferrer"
+            >Route</a>
+            {/*{[*/}
+            {/*    transportImg(require("./walk.svg"), "Walking", routeLink(myLoc, lat, lon, "walking")),*/}
+            {/*    transportImg(require("./bike.svg"), "Bike", routeLink(myLoc, lat, lon, "bicycling")),*/}
+            {/*    transportImg(require("./transit.svg"), "Transit", routeLink(myLoc, lat, lon, "transit")),*/}
+            {/*    transportImg(require("./car.svg"), "Car", routeLink(myLoc, lat, lon, "driving")),*/}
+            {/*]}*/}
         </div>);
     };
 
