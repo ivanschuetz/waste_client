169128bb2d47991c88158f450448c83b5ab9b1b4
khimsh <i.khimsh@gmail.com>
2019-07-19 09:05:33 +0400

    updated checkboxes

--- src/RecipientsMap.js
+++ src/RecipientsMap.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './App.css';
-import {Map, Marker, Popup, TileLayer} from "react-leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import L from 'leaflet';
 import Supercluster from 'supercluster';
 import OpeningHours from "./OpeningHours";
-import {isOpenNow} from "./Time";
-import {useTranslation} from "react-i18next";
-import {routeLink} from "./Utils";
+import { isOpenNow } from "./Time";
+import { useTranslation } from "react-i18next";
+import { routeLink } from "./Utils";
 
 const makeIcon = (path, iconAnchor, iconSize, popupAnchor) => new L.Icon({
     iconUrl: path,
@@ -45,7 +45,7 @@ const createClusterIcon = (count) => {
 
 // https://developers.google.com/maps/documentation/urls/guide
 
-const RecipientsMap = ({recipients}) => {
+const RecipientsMap = ({ recipients }) => {
     const [myLoc, setMyLoc] = useState(null);
     const [zoom, setZoom] = useState(11);
     const [filteredRecipients, setFilteredRecipients] = useState(recipients);
@@ -60,7 +60,7 @@ const RecipientsMap = ({recipients}) => {
     });
 
     const myRef = useRef(null);
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     // Scroll such that maps becomes fully visible when loading component
     useEffect(() => myRef.current && window.scrollTo(0, myRef.current.offsetTop), []);
@@ -79,22 +79,22 @@ const RecipientsMap = ({recipients}) => {
 
     const transportImg = (src, alt, link) =>
         <a href={link} target="_blank" rel="noopener noreferrer" key={alt}>
-            <img src={src} alt={alt} style={{width: 20, height: 20, marginRight: 20}}/>
+            <img src={src} alt={alt} style={{ width: 20, height: 20, marginRight: 20 }} />
         </a>;
 
     const points = filteredRecipients.map((container) => {
-            return {
-                properties: {
-                    container: container
-                },
-                geometry: {
-                    coordinates: [
-                        container["lat"],
-                        container["lon"]
-                    ]
-                }
-            };
-        }
+        return {
+            properties: {
+                container: container
+            },
+            geometry: {
+                coordinates: [
+                    container["lat"],
+                    container["lon"]
+                ]
+            }
+        };
+    }
     );
 
     const onClusterClick = (cluster) => {
@@ -109,7 +109,7 @@ const RecipientsMap = ({recipients}) => {
             case "u":
                 return <div className="always-open-label">{t('map_open_unknown')}</div>;
             case "h":
-                return <OpeningHours openingHoursList={open["hours"]} mode="map"/>;
+                return <OpeningHours openingHoursList={open["hours"]} mode="map" />;
             default:
                 console.log('Unknown openType value: ' + JSON.stringify(openType));
         }
@@ -146,19 +146,19 @@ const RecipientsMap = ({recipients}) => {
         const lon = container["lon"];
         const phone = container["phone"];
 
-        return marker(lat, lon, icon(container["type"], isOpen(container).isOpen, container["hasPickup"]), <div style={{minWidth: 200}}>
-        {/*return marker(lat, lon, isOpen(container) ? markerOpenIcon : markerClosedIcon, <div style={{minWidth: 200}}>*/}
+        return marker(lat, lon, icon(container["type"], isOpen(container).isOpen, container["hasPickup"]), <div style={{ minWidth: 200 }}>
+            {/*return marker(lat, lon, isOpen(container) ? markerOpenIcon : markerClosedIcon, <div style={{minWidth: 200}}>*/}
             <a className="p-container-popup-title" href={container["url"]} target="_blank" rel="noopener noreferrer">
                 {container["name"]}
-            </a><br/>
-            {container["address"]}<br/>
-            {phone ? <span>{container["phone"]}</span> : <span/>}
+            </a><br />
+            {container["address"]}<br />
+            {phone ? <span>{container["phone"]}</span> : <span />}
             {openElement(container["openType"], container["open"])}
             {/*<a className="p-container-popup-company" href={container["url"]} target="_blank">*/}
             {/*    {container["company"]}*/}
             {/*</a>*/}
-            <div className="p-container-popup-routes"/>
-            <span style={{float: "left", marginRight: 5}}>Route:</span>
+            <div className="p-container-popup-routes" />
+            <span style={{ float: "left", marginRight: 5 }}>Route:</span>
             {[
                 transportImg(require("./walk.svg"), "Walking", routeLink(myLoc, lat, lon, "walking")),
                 transportImg(require("./bike.svg"), "Bike", routeLink(myLoc, lat, lon, "bicycling")),
@@ -175,7 +175,7 @@ const RecipientsMap = ({recipients}) => {
         const count = result["properties"]["point_count"];
         const countText = count > 999 ? "+" : count;
         return <Marker position={[lat, lng]} onClick={() => onClusterClick(result)}
-                       icon={createClusterIcon(countText)}/>
+            icon={createClusterIcon(countText)} />
     };
 
     const onZoomEvent = (event) => {
@@ -251,64 +251,82 @@ const RecipientsMap = ({recipients}) => {
     return (
         <div className='map-with-filters-container'>
             <div className='map-filters'>
-                <div>
-                    <label className='map-filter'>
-                        {t('map_filter_disposal_places')}
+                <div className="map-filters-wrapper">
+                    <div>
                         <input
+                            id="check-1"
                             name="showDisposalPlaces"
                             type="checkbox"
                             checked={filterOptions.showDisposalPlaces}
-                            onChange={updateFilterOptions}/>
-                    </label>
-                    <label className='map-filter'>
-                        {t('map_filter_donation_places')}
+                            onChange={updateFilterOptions} />
+                        <label className='map-filter' htmlFor="check-1">
+                            {t('map_filter_disposal_places')}
+                        </label>
+                    </div>
+                    <div>
                         <input
+                            id="check-2"
                             name="showDonationPlaces"
                             type="checkbox"
                             checked={filterOptions.showDonationPlaces}
-                            onChange={updateFilterOptions}/>
-                    </label>
-                    <label className='map-filter'>
-                        {t('map_filter_2hand_places')}
+                            onChange={updateFilterOptions} />
+                        <label className='map-filter' htmlFor="check-2">
+                            {t('map_filter_donation_places')}
+
+                        </label>
+                    </div>
+                    <div>
                         <input
+                            id="check-3"
                             name="showSecondHandPlaces"
                             type="checkbox"
                             checked={filterOptions.showSecondHandPlaces}
-                            onChange={updateFilterOptions}/>
-                    </label>
-                    <label className='map-filter'>
-                        {t('map_filter_retailers')}
+                            onChange={updateFilterOptions} />
+                        <label className='map-filter' htmlFor="check-3">
+                            {t('map_filter_2hand_places')}
+                        </label>
+                    </div>
+                    <div>
                         <input
+                            id="check-4"
                             name="showRetailers"
                             type="checkbox"
                             checked={filterOptions.showRetailers}
-                            onChange={updateFilterOptions}/>
-                    </label>
+                            onChange={updateFilterOptions} />
+                        <label className='map-filter' htmlFor="check-4">
+                            {t('map_filter_retailers')}
+                        </label>
+                    </div>
                 </div>
-                <div  style={{marginTop: 10}}>
-                    <label className='map-filter' style={{marginLeft: 10}}>
-                        {t('map_filter_has_pickup')}
+                <div style={{ marginTop: 20 }} className="map-filters-wrapper">
+                    <div>
                         <input
+                            id="check-5"
                             name="showHasPickup"
                             type="checkbox"
                             checked={filterOptions.showHasPickup}
-                            onChange={updateFilterOptions}/>
-                    </label>
-                    <label className='map-filter'>
-                        {t('map_filter_has_in_place')}
+                            onChange={updateFilterOptions} />
+                        <label className='map-filter' style={{ marginLeft: 10 }} htmlFor="check-5">
+                            {t('map_filter_has_pickup')}
+                        </label>
+                    </div>
+                    <div>
                         <input
+                            id="check-6"
                             name="showHasInPlace"
                             type="checkbox"
                             checked={filterOptions.showHasInPlace}
-                            onChange={updateFilterOptions}/>
-                    </label>
-
+                            onChange={updateFilterOptions} />
+                        <label className='map-filter' htmlFor="check-6">
+                            {t('map_filter_has_in_place')}
+                        </label>
+                    </div>
                 </div>
             </div>
 
             <div className='map-container' ref={myRef}>
-                <Map center={[52.520008, 13.404954]} zoom={zoom} maxZoom={18} ref={map} style={{height: 380}}
-                     onZoomend={onZoomEvent}>
+                <Map center={[52.520008, 13.404954]} zoom={zoom} maxZoom={18} ref={map} style={{ height: 380 }}
+                    onZoomend={onZoomEvent}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
